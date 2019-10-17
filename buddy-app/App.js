e03dd4f989f590d4d814d5719e5adbaabb281175
Kevin Tou <toukevin@gmail.com>
2019-10-17 09:49:22 -0700

    Created a basic version of the sign in page

--- buddy-app/App.js
+++ buddy-app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SignIn from './components/SignIn';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SignIn />
     </View>
   );
 }
