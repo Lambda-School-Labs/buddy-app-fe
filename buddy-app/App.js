967680aace554617feb01f28e93f089cbbc99439
Kevin Tou <toukevin@gmail.com>
2019-10-21 11:39:43 -0700

    Added basic navigation

--- buddy-app/App.js
+++ buddy-app/App.js
@@ -1,50 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View } from 'react-native';
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
+import React from 'react';
 
-
-import Landing from './components/Landing'
-
-// components 
-import SignUp from './components/SignUp.js'; 
-// import SignIn from './components/SignIn';
+// components
+import AppNavigator from './components/AppNavigator';
 
 export default function App() {
-  const [isAppReady, setAppReady] = useState(false)
-
-  return (
-    isAppReady === false ? (
-      <AppLoading startAsync={_cacheResourcesAsync}
-        onFinish={() => setAppReady(true)}
-        onError={console.warn} 
-        />
-    ) : (
-      <View style={styles.container}>
-        {/* <Image source={require('./assets/goose-test.png')} /> */}
-        <SignUp />
-        {/* <SignIn /> */}
-      </View>
-    )
-  );
+  return <AppNavigator />;
 }
-
-
-const  _cacheResourcesAsync = async () => {
-  const images = [
-    require('./assets/goose-test.png')
-  ];
-  const cacheImages = images.map(image => {
-    return Asset.fromModule(image).downloadAsync();
-  }); 
-  return Promise.all(cacheImages);
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
--- buddy-app/components/Landing.js
+++ buddy-app/components/Landing.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Alert, Text, View, Button } from 'react-native';
 
-export default function Landing() {
-
+export default function Landing(props) {
   return (
     <View>
-    <View><Text>A friendly network to help you discover the world around.</Text></View>
-      
       <View>
-      <Button title="Sign In" onPress={() => Alert.alert('Sign In pressed!')}/>
-      <Button title="Sign Up" onPress={() => Alert.alert('Sign Up pressed!')}/>
+        <Text>A friendly network to help you discover the world around.</Text>
+      </View>
+
+      <View>
+        <Button
+          title="Sign In"
+          onPress={() => props.navigation.navigate('SignIn')}
+        />
+        <Button
+          title="Sign Up"
+          onPress={() => props.navigation.navigate('SignUp')}
+        />
       </View>
     </View>
   );
