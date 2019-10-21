4aba767aac4120b50a42d3b961c6b5bd10d8f410
Tommy Coleman <tommiilee@gmail.com>
2019-10-21 15:06:41 -0400

    laid out bare bones redux

--- buddy-app/App.js
+++ buddy-app/App.js
@@ -2,28 +2,35 @@ import React, { useState } from 'react';
 import { StyleSheet, Image, View } from 'react-native';
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
-
-import Landing from './components/Landing'
-
 // components 
 import SignUp from './components/SignUp.js'; 
 import SignIn from './components/SignIn';
+import Landing from './components/Landing'
+//global state
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import BuddyReducer from './reducers/BuddyReducer';
+const store = createStore(BuddyReducer)
 
 export default function App() {
   const [isAppReady, setAppReady] = useState(false)
 
   return (
     isAppReady === false ? (
+    
       <AppLoading startAsync={_cacheResourcesAsync}
         onFinish={() => setAppReady(true)}
         onError={console.warn} 
         />
+        
     ) : (
+      <Provider store={ store }>
       <View style={styles.container}>
         <Image source={require('./assets/goose-test.png')} />
         <SignUp />
         <SignIn />
       </View>
+      </Provider>
     )
   );
 }
---
+++ buddy-app/reducers/BuddyReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    token: '',
+}
+
+export const buddyReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default: return state;
+    }
+}
