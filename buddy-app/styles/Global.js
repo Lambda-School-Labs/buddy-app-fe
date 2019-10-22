ec0354c50cda86122fa78978fb1f4ae4c7986e03
justinelai <justine.lai@gmail.com>
2019-10-22 17:03:05 -0400

    refactor inputs and padding for SignUp and SignIn

--- buddy-app/styles/Global.js
+++ buddy-app/styles/Global.js
@@ -11,6 +11,33 @@ const Global = StyleSheet.create({
         borderBottomWidth: 5,
         borderBottomColor: 'black',
     },
+    title: {
+        marginTop: 60,
+        fontSize: 30,
+        color: Colors.darkGray,
+        fontFamily: 'Nunito-Regular',
+    },
+    container: {
+        flex: 1,
+        paddingHorizontal: 30,
+        width: '100%',
+        marginTop: 55,
+        alignItems: 'flex-start',
+    },
+    formContainer: {
+        width: '100%',
+        marginTop: 30,
+    },
+    input: {
+        borderColor: Colors.lightGray,
+        borderWidth: 1,
+        width: '100%',
+        height: 40,
+        paddingLeft: 10,
+        marginBottom: 20,
+        fontSize: 15,
+    },
+    
 })
 
-export default Global
+export default Global;
