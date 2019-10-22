1f72ea11a20dd012314575058393e1a162c14343
May Ng <mngmay@gmail.com>
2019-10-22 09:33:33 -0700

    testing

--- buddy-app/App.js
+++ buddy-app/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
 // components
-import AppNavigator from './components/AppNavigator';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import {buddyReducer}  from './reducers/BuddyReducer';
-const store = createStore(buddyReducer)
+import AppNavigator from "./components/AppNavigator";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { buddyReducer } from "./reducers/BuddyReducer";
+const store = createStore(buddyReducer);
 
 export default function App() {
-  return <Provider store={store}><AppNavigator /></Provider>;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
