36238731c025483b6b4bc0e2efefd1d919ef3905
May Ng <mngmay@gmail.com>
2019-10-23 14:37:13 -0700

    Created plain dashboard layout

--- buddy-app/components/Dashboard.js
+++ buddy-app/components/Dashboard.js
@@ -1,15 +1,49 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  AsyncStorage
+} from "react-native";
 import { connect } from "react-redux";
+
+//styles
+import Buttons from "../styles/Buttons";
+import Global from "../styles/Global";
+
 const Dashboard = props => {
   return (
-    <View>
-      <Text>
-        Welcome {props.user.first_name} {props.user.last_name}!
-      </Text>
+    <View style={Global.container}>
+      <View style={Global.logoContainer}>
+        <Text style={Global.logo}>BUDDY</Text>
+      </View>
+      <View style={styles.bottomNav}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  bottomNav: {
+    backgroundColor: "#6d6dff",
+    height: 96,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  fakeLink: {
+    color: "#6D6DFF",
+    textDecorationLine: "underline",
+    fontSize: 15,
+    fontFamily: "Nunito-Light"
+  },
+  fakeLinkContainer: {
+    alignSelf: "center"
+  }
+});
 
 const mapStateToProps = state => {
   return {
