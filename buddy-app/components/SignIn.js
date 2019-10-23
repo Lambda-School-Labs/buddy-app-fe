323130d164a6314c0dde08c896f9081aa5d89687
Kevin Tou <toukevin@gmail.com>
2019-10-23 14:24:44 -0700

    Included validation for the sign in

--- buddy-app/components/SignIn.js
+++ buddy-app/components/SignIn.js
@@ -31,14 +31,10 @@ class SignIn extends ValidationComponent {
   };
 
   cancelSignInHandler = () => {
-    this.setState({ email: '', password: '' });
     this.props.navigation.navigate('Landing');
   };
 
   signInHandler = () => {
-    if (!this.state.email || !this.state.password) {
-      return;
-    }
     this.props.isLoadingPage(true);
 
     axios
@@ -53,11 +49,22 @@ class SignIn extends ValidationComponent {
         this.props.navigation.navigate('AuthStack');
       })
       .catch(err => {
-        console.log(err.message);
+        this.props.isLoadingPage(false);
+        this.onComplete();
+        Alert.alert('Warning', 'Invalid credentials.', [{ text: 'OK' }]);
       });
-    // Validation
-    // Performs a HTTP request to the backend
-    // Returns...?
+  };
+
+  onComplete = () => {
+    this.validate({
+      email: {
+        email: true,
+        required: true,
+      },
+      password: {
+        required: true,
+      },
+    });
   };
 
   render() {
@@ -77,6 +84,13 @@ class SignIn extends ValidationComponent {
                 value={this.state.email}
                 autoCapitalize="none"
               />
+              {this.isFieldInError('email') &&
+                this.getErrorsInField('email').map(errorMessage => (
+                  <Text style={styles.error} key={errorMessage}>
+                    {errorMessage}
+                  </Text>
+                ))}
+
               <TextInput
                 style={Global.input}
                 placeholder="Password"
@@ -85,6 +99,13 @@ class SignIn extends ValidationComponent {
                 autoCapitalize="none"
                 secureTextEntry
               />
+              {this.isFieldInError('password') &&
+                this.getErrorsInField('password').map(errorMessage => (
+                  <Text style={styles.error} key={errorMessage}>
+                    {errorMessage}
+                  </Text>
+                ))}
+
               <View style={styles.fakeLinkContainer}>
                 <Text
                   style={styles.fakeLink}
@@ -117,97 +138,6 @@ class SignIn extends ValidationComponent {
   }
 }
 
-// const SignIn = props => {
-//   const [info, setInfo] = useState({ email: "", password: "" });
-//   const changeHandler = (value, name) => {
-//     setInfo({ ...info, [name]: value });
-//   };
-
-//   const cancelSignInHandler = () => {
-//     setInfo({ email: "", password: "" });
-//     props.navigation.navigate("Landing");
-//   };
-
-//   const signInHandler = () => {
-//     if (!info.email || !info.password) {
-//       return;
-//     }
-//     props.isLoadingPage(true);
-
-//     axios
-//       .post("https://buddy-app-be.herokuapp.com/auth/signin", info)
-//       .then(res => {
-//         storeToken(res.data.token);
-//         props.addUser({
-//           first_name: res.data.first_name,
-//           last_name: res.data.last_name,
-//           id: res.data.id
-//         });
-//         props.navigation.navigate("AuthStack");
-//       })
-//       .catch(err => {
-//         console.log(err.message);
-//       });
-//     // Validation
-//     // Performs a HTTP request to the backend
-//     // Returns...?
-//   };
-
-//   if (!props.isLoading && props.user.id === "") {
-//     return (
-//       <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }}>
-//         <View style={Global.container}>
-//           <View style={Global.logoContainer}>
-//             <Text style={Global.logo}>BUDDY</Text>
-//           </View>
-//           <Text style={Global.title}>Sign In</Text>
-//           <View style={Global.formContainer}>
-//             <TextInput
-//               style={Global.input}
-//               placeholder="Email"
-//               onChangeText={e => changeHandler(e, "email")}
-//               value={info.email}
-//               autoCapitalize="none"
-//             />
-//             <TextInput
-//               style={Global.input}
-//               placeholder="Password"
-//               onChangeText={e => changeHandler(e, "password")}
-//               value={info.password}
-//               autoCapitalize="none"
-//               secureTextEntry
-//             />
-//             <View style={styles.fakeLinkContainer}>
-//               <Text
-//                 style={styles.fakeLink}
-//                 onPress={() => {
-//                   props.navigation.navigate("SignUp");
-//                 }}
-//               >
-//                 Don't have an account yet? Sign Up
-//               </Text>
-//             </View>
-//             <View style={Buttons.container}>
-//               <TouchableOpacity onPress={cancelSignInHandler}>
-//                 <Text style={[Buttons.text, Buttons.textAuth]}>Cancel</Text>
-//               </TouchableOpacity>
-//               <TouchableOpacity
-//                 onPress={signInHandler}
-//                 style={[Buttons.btn, Buttons.secondary, { width: 130 }]}
-//               >
-//                 <Text style={[Buttons.text, Buttons.textAuth]}>Sign In</Text>
-//               </TouchableOpacity>
-//             </View>
-//           </View>
-//           <View style={styles.bottomNav}></View>
-//         </View>
-//       </KeyboardAwareScrollView>
-//     );
-//   } else {
-//     return <Spinner visible={props.isLoading} textContent={"Loading...."} />;
-//   }
-// };
-
 const styles = StyleSheet.create({
   bottomNav: {
     backgroundColor: '#6d6dff',
@@ -225,6 +155,9 @@ const styles = StyleSheet.create({
   },
   fakeLinkContainer: {
     alignSelf: 'center',
+  },
+  error: {
+    color: '#a80000',
   },
 });
 
