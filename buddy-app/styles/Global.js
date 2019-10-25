ec047f7cb3fa9017cee911fe7e74c3b55b05b4bc
justinelai <justine.lai@gmail.com>
2019-10-25 18:58:36 -0400

    fontSize

--- buddy-app/styles/Global.js
+++ buddy-app/styles/Global.js
@@ -1,55 +1,55 @@
-import { StyleSheet } from 'react-native';
-import Colors from './Colors.js'
+import { StyleSheet } from "react-native";
+import Colors from "./Colors.js";
 
 const Global = StyleSheet.create({
-    logo: { fontSize: 35,
-        color: Colors.darkGray,
-        fontFamily: 'Nunito-Black',
-        marginBottom: -5,
-    },
-    logoContainer: {
-        borderBottomWidth: 5,
-        borderBottomColor: 'black',
-    },
-    title: {
-        marginTop: 60,
-        fontSize: 30,
-        color: Colors.darkGray,
-        fontFamily: 'Nunito-Regular',
-    },
-    textNormal: {
-        fontSize: 25,
-        color: Colors.darkGray,
-        fontFamily: 'Nunito-Regular',
-    },
-    container: {
-        flex: 1,
-        paddingHorizontal: 30,
-        width: '100%',
-        marginTop: 55,
-        alignItems: 'flex-start',
-    },
-    formContainer: {
-        width: '100%',
-        marginTop: 30,
-    },
-    input: {
-        borderColor: Colors.lightGray,
-        borderWidth: 1,
-        width: '100%',
-        height: 40,
-        paddingLeft: 10,
-        marginTop: 20,
-        fontSize: 15,
-        fontFamily: 'Nunito-Light',
-        borderRadius: 5
-    },
-    error: {
-        color: "#a80000",
-        fontFamily: "Nunito-Light",
-        fontSize: 12
-    }
-    
-})
+  logo: {
+    fontSize: 35,
+    color: Colors.darkGray,
+    fontFamily: "Nunito-Black",
+    marginBottom: -5
+  },
+  logoContainer: {
+    borderBottomWidth: 5,
+    borderBottomColor: "black"
+  },
+  title: {
+    marginTop: 60,
+    fontSize: 30,
+    color: Colors.darkGray,
+    fontFamily: "Nunito-Regular"
+  },
+  textNormal: {
+    fontSize: 25,
+    color: Colors.darkGray,
+    fontFamily: "Nunito-Regular"
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    width: "100%",
+    marginTop: 55,
+    alignItems: "flex-start"
+  },
+  formContainer: {
+    width: "100%",
+    marginTop: 30
+  },
+  input: {
+    borderColor: Colors.lightGray,
+    borderWidth: 1,
+    width: "100%",
+    height: 40,
+    paddingLeft: 10,
+    marginTop: 20,
+    fontSize: 15,
+    fontFamily: "Nunito-Regular",
+    borderRadius: 5
+  },
+  error: {
+    color: "#a80000",
+    fontFamily: "Nunito-Light",
+    fontSize: 14
+  }
+});
 
 export default Global;
