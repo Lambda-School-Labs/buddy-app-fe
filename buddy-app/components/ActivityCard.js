fec7fcd60ea222d227fe76446eee07ff9182de58
May Ng <mngmay@gmail.com>
2019-11-12 13:23:59 -0800

    Have posting guests to activities and filter out attending activities.  Need to refactor to filter out at dashboard component to prevent initial render.

--- buddy-app/components/ActivityCard.js
+++ buddy-app/components/ActivityCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
 import EditActivity from "./EditActivity";
 import { connect } from "react-redux";
@@ -12,13 +12,8 @@ import { getToken } from "../utils/authHelper";
 
 function ActivityCard(props) {
   const [activity] = useState(props.activity);
-
+  const [guestList, setGuestList] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const joinedActivity = {
-    user_id: props.user.id,
-    activity_id: activity.id
-  };
 
   const toggleModal = () => {
     // console.log(props);
@@ -26,7 +21,7 @@ function ActivityCard(props) {
   };
 
   // post the activity to user's activities table
-  handleJoin = () => {
+  handleJoin = joinedActivity => {
     getToken().then(token => {
       axiosWithAuth(token)
         .post(
@@ -34,57 +29,97 @@ function ActivityCard(props) {
           joinedActivity
         )
         .then(res => {
-          console.log("posted", res.data);
+          console.log("posted", res);
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+          console.log(activity, joinedActivity);
+          console.log(err);
+        });
     });
   };
 
-  // console.log("activity", activity);
+  useEffect(() => {
+    getToken().then(token => {
+      axiosWithAuth(token)
+        //get length of activity guest list.
+        .get(
+          `https://buddy-app-be.herokuapp.com/useractivities/activity/${activity.id}`
+        )
+        .then(res => {
+          // console.log("GuestList res.data", res.data);
+          if (res.data.length > 0) {
+            const guestList = res.data.map(activity => activity.user_id);
+            setGuestList(guestList);
+          }
+        })
+        .catch(err => console.log(err));
+    });
+  }, []);
+
+  console.log("GuestList", guestList);
+
+  console.log("activity", activity);
   // console.log("User", props.user);
-  console.log("props", props);
-  console.log("joinedActivity", joinedActivity);
+  // console.log("props", props);
+  // console.log("joinedActivity", joinedActivity);
 
-  return (
-    <View style={styles.activityCard}>
-      <View style={styles.activityView}>
-        <Text style={styles.activityText}>
-          <Text style={{ fontFamily: "Nunito-Bold" }}>{activity.name}</Text> on{" "}
-          <Text style={{ fontFamily: "Nunito-Bold" }}>{activity.date}</Text> at{" "}
-          <Text style={{ fontFamily: "Nunito-Bold" }}>{activity.time}</Text>{" "}
-          with {activity.organizer_name}
-        </Text>
+  if (
+    (guestList.includes(props.user.id) === false &&
+      activity.guest_limit === null) ||
+    guestList.length < activity.guest_limit
+  ) {
+    return (
+      <View style={styles.activityCard}>
+        <View style={styles.activityView}>
+          <Text style={styles.activityText}>
+            <Text style={{ fontFamily: "Nunito-Bold" }}>{activity.name}</Text>{" "}
+            on{" "}
+            <Text style={{ fontFamily: "Nunito-Bold" }}>{activity.date}</Text>{" "}
+            at{" "}
+            <Text style={{ fontFamily: "Nunito-Bold" }}>{activity.time}</Text>{" "}
+            with {activity.organizer_name}
+          </Text>
+        </View>
+
+        {props.user.id === props.activity.organizer_id ? (
+          <View style={[Buttons.activityBtn, Buttons.primary, Buttons.editBtn]}>
+            <TouchableOpacity
+              onPress={toggleModal}
+              style={{
+                width: "100%",
+                height: 40,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text style={Buttons.textWhite}>Edit</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={[Buttons.activityBtn]}>
+            <TouchableOpacity
+              onPress={() => {
+                handleJoin({
+                  user_id: props.user.id,
+                  activity_id: props.activity.id
+                });
+              }}
+            >
+              <Text style={Buttons.text}>Ask to Join</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        <EditActivity
+          activity={activity}
+          isModalVisible={isModalVisible}
+          toggleModal={toggleModal}
+          toggleState={props.setRerender}
+        />
       </View>
-
-      {props.user.id === props.activity.organizer_id ? (
-        <View style={[Buttons.activityBtn, Buttons.primary, Buttons.editBtn]}>
-          <TouchableOpacity
-            onPress={toggleModal}
-            style={{
-              width: "100%",
-              height: 40,
-              justifyContent: "center",
-              alignItems: "center"
-            }}
-          >
-            <Text style={Buttons.textWhite}>Edit</Text>
-          </TouchableOpacity>
-        </View>
-      ) : (
-        <View style={[Buttons.activityBtn]}>
-          <TouchableOpacity onPress={() => handleJoin()}>
-            <Text style={Buttons.text}>Ask to Join</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      <EditActivity
-        activity={activity}
-        isModalVisible={isModalVisible}
-        toggleModal={toggleModal}
-        toggleState={props.setRerender}
-      />
-    </View>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 
 const styles = StyleSheet.create({
