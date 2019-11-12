7323b4c38c14d635b95ba58f34e5270a4c7dfe3c
justinelai <justine.lai@gmail.com>
2019-11-12 17:12:31 -0500

    build profile structure

--- buddy-app/components/Profile.js
+++ buddy-app/components/Profile.js
@@ -1,33 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { addUser } from "../actions/buddyActions";
-import Global from "../styles/Global";
-import { onSignOut } from "../utils/authHelper";
 import {
   View,
-  TouchableHighlight,
   Text,
-  Image,
   TouchableOpacity,
-  AsyncStorage,
   ScrollView,
   StyleSheet
 } from "react-native";
-import { Feather } from "@expo/vector-icons";
 
+// Components
 import ProfileModal from "./ProfileModal";
+import ProfileHighlight from "./ProfileHighlight";
+import ProfileCard from "./ProfileHighlight";
 import { NavBar } from "./NavBar";
 
+// Styles
+import { Feather } from "@expo/vector-icons";
+import Global from "../styles/Global";
+import Colors from "../styles/Colors";
+
 const Profile = props => {
+  // ---------- MODAL ----------- //
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const toggleModal = () => setIsModalVisible(!isModalVisible);
+  const sendToLanding = () => props.navigation.navigate("Landing"); // pass nav props since React Navigation Modal doesn't play nicely with React Navigation :-\
 
-  const toggleModal = () => {
-    setIsModalVisible(!isModalVisible);
-  };
-
-  const sendToLanding = () => {
-    props.navigation.navigate("Landing");
-  };
+  // ---------- activities ---------- //
+  // get activities where user is organizer
+  // get activities where user is guest
+  // separate top 3 activities for ProfileHighlight box
+  // remaining activities are made into ProfileCard components
 
   return (
     <View style={Global.container}>
@@ -39,12 +42,22 @@ const Profile = props => {
           <Feather name="user" size={30} color="#000" />
         </TouchableOpacity>
       </View>
-      <View>
-        <Text style={[Global.textNormal, { marginTop: 20 }]}>
-          Hi, {props.user.firstname}
-        </Text>
-        <View style={{ height: "70%" }}>
-          <ScrollView></ScrollView>
+      <View style={styles.profileBody}>
+        <Text style={styles.title}>Hi, {props.user.first_name}</Text>
+        <View style={styles.hr} />
+        <View>
+          <ScrollView>
+            <Text style={styles.subtitle}>Upcoming Activities</Text>
+            <ProfileHighlight />
+            <View style={styles.activityCardList}>
+              {/* map over remaining activities and generate <ProfileCard /> for each */}
+            </View>
+            <View style={styles.profileCounter}>
+              <Text style={styles.subtitle}>What I've Been Up To</Text>
+              <Text style={styles.text}>Total Activities:</Text>
+              <Text style={styles.textBold}>[Count] Activities</Text>
+            </View>
+          </ScrollView>
         </View>
       </View>
       <NavBar navigation={props.navigation} />
@@ -63,6 +76,46 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center"
+  },
+  profileBody: {
+    width: "100%"
+  },
+  hr: {
+    borderBottomColor: Colors.lightGray,
+    borderBottomWidth: 1,
+    marginVertical: 20,
+    width: "100%"
+  },
+  title: {
+    fontSize: 25,
+    marginTop: 30,
+    fontFamily: "Nunito-Regular",
+    color: Colors.darkGray
+  },
+  subtitle: {
+    fontSize: 20,
+    fontFamily: "Nunito-Bold",
+    color: Colors.darkGray,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    fontFamily: "Nunito-Regular",
+    color: Colors.darkGray
+  },
+  textBold: {
+    fontSize: 16,
+    fontFamily: "Nunito-Bold",
+    color: Colors.darkGray
+  },
+  activityCardList: {
+    height: "auto",
+    width: "100%",
+    marginTop: 30
+  },
+  profileCounter: {
+    paddingVertical: 20,
+    height: "auto"
   }
 });
 
