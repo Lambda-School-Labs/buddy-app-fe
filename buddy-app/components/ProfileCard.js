111f3de178a44931ae3802e539ffc6beb4cdc272
justinelai <justine.lai@gmail.com>
2019-11-13 16:03:11 -0500

    style profile and convert dates

--- buddy-app/components/ProfileCard.js
+++ buddy-app/components/ProfileCard.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Colors from "../styles/Colors";
+import moment from "moment";
 
 const ProfileCard = props => {
   const { activity } = props;
+  const convertedDay = moment(activity.date, "MM-DD-YY").format("MMM Do");
+
   console.log("profile card is being rendered");
   return (
     <View style={styles.card}>
-      <Text style={[styles.textBold, { fontSize: 18 }]}>{activity.date}</Text>
-      <Text style={[styles.text, { fontSize: 18 }]}>
-        {activity.name} at {activity.time} with Person
-      </Text>
+      <Text style={styles.textBold}>{convertedDay}</Text>
+      <View style={styles.textBox}>
+        <Text style={styles.text}>
+          {activity.name} at {activity.time}
+        </Text>
+      </View>
     </View>
   );
 };
@@ -19,12 +24,26 @@ const styles = StyleSheet.create({
   card: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
+    alignItems: "flex-start",
     borderBottomWidth: 1,
     borderBottomColor: Colors.lightGray,
     width: "100%",
     height: "auto",
     paddingVertical: 10
+  },
+  text: {
+    fontSize: 16,
+    fontFamily: "Nunito-Regular",
+    color: Colors.darkGray
+  },
+  textBold: {
+    fontSize: 16,
+    fontFamily: "Nunito-Bold",
+    color: Colors.darkGray
+  },
+  textBox: {
+    width: "75%",
+    alignItems: "flex-end"
   }
 });
 
